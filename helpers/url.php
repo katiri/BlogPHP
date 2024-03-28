521e831a1c094756a46a96e70eab074fcd4d327d
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-28 09:14:50 -0300

    Configurações iniciais do projeto, criação do index.php com a estrutura inicial do HTML, importação da fonte Montserrat e do arquivo local de css styles.css e configuração do helper url para importação de arquivos estáticos no HTML

---
+++ helpers/url.php
@@ -0,0 +1,3 @@
+<?php
+    // Isso vai me ajudar a linkar arquivos estáticos com o HTML do projeto
+    $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI'] . "?") . "/";
