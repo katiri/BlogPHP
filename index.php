8877dff8ac98e964c9cd23a3332516e162475295
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-03-28 10:17:16 -0300

    Correção na página principal e definição dos estilos da página principal

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     </div>
     <div id="posts-container">
         <?php foreach($posts as $post): ?>
-            <div id="post-box"> 
+            <div class="post-box"> 
                 <img src="<?= $BASE_URL ?>img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
                 <h2 class="post-title">
                     <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>">
